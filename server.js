258d23e342b43b335ac54934e13f0380e1aebb47
Priyanshu Dubey <lcb2021003@iiitl.ac.in>
2024-08-13 22:02:10 +0530

    Adding routes layout in server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const PORT = 3000;
+const ruleRoutes = require('./routes/ruleRoutes');
 
 // express app
 const app = express();
@@ -13,12 +14,14 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.json())
 
+// calling apis 
+app.use('/api/rules', ruleRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+      console.log('connected to db & listening on port', process.env.PORT || PORT)
     })
   })
   .catch((error) => {
